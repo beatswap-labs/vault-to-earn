7d270cd0d59665f98cfe093023acc21ff901f213
BeatSwap <beatxswap@beatswap.io>
2025-11-15 16:45:17 +0900

    Update iplicensingvault.basic.test.js

--- bnb/test/iplicensingvault.basic.test.js
+++ bnb/test/iplicensingvault.basic.test.js
@@ -11,22 +11,27 @@ describe("IPLicensingVault basic flows", function () {
       "mUSDT",
       ethers.parseUnits("1000000", 18)
     );
+    await usdt.waitForDeployment();
+
     const btx = await MockERC20.deploy(
       "Mock BTX",
       "mBTX",
       ethers.parseUnits("1000000", 18)
     );
+    await btx.waitForDeployment();
 
     const Vault = await ethers.getContractFactory("IPLicensingVault");
     const depositCap = ethers.parseUnits("1000", 18);
 
     const vault = await Vault.deploy(
-      usdt.address,
-      btx.address,
+      usdt.target,
+      btx.target,
       treasury.address,
       depositCap
     );
+    await vault.waitForDeployment();
 
+    const usdtAddress = await usdt.getAddress();
     await usdt.transfer(user.address, ethers.parseUnits("1000", 18));
 
     return { deployer, user, treasury, usdt, btx, vault, depositCap };
@@ -36,7 +41,7 @@ describe("IPLicensingVault basic flows", function () {
     const { user, usdt, vault } = await deployFixture();
 
     const amount = ethers.parseUnits("100", 18);
-    await usdt.connect(user).approve(vault.address, amount);
+    await usdt.connect(user).approve(vault.target, amount);
 
     await expect(vault.connect(user).deposit(amount))
       .to.emit(vault, "Deposit")
